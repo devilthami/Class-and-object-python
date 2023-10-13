3940231cefeaf6ed1b359d4dacdb41980049b364
P@rashmgr <96672535+devilthami@users.noreply.github.com>
2023-10-13 21:20:32 +0545

    Add files via upload

---
+++ Class-case.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age, book):
+        self.name = name
+        self.age = age
+        self.book = book
+
+
+p1 = Person("Parash", 25, "harry-potter")
+
+print(p1)
---
+++ class3.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+
+p1 = Person("Parash", 25)
+
+print(p1)
